daab265ae5df0376d049d7bfe58ddda192b82f9f
atifwebdev <126596363+atifwebdev@users.noreply.github.com>
2023-04-10 15:05:37 +0500

    js assign 1 complete

---
+++ assign/app.js
@@ -0,0 +1,95 @@
+// let num1 = +prompt("Enter number one");
+// let num2 = +prompt("Enter number two");
+// if (num1 > num2) {
+//     console.log("First input: " + num1);
+// }   else if (num2 > num1) {
+//     console.log("Second input: " + num2);
+// }   else{
+//     console.log("Kindly enter diffrent number in both inputs " + "Input 1 num " + num1 + "Input 2 num " + num2 );
+// }
+
+
+// let numberOne = +prompt("Enter any number to find its sign + or -");
+// if (numberOne >= 0) {
+//     alert ("The sign is + " + numberOne);
+// }   else{
+//     alert ("The sign is - " + numberOne);
+// }
+
+
+// let number1 = +prompt("Enter number 1");
+// let number2 = +prompt("Enter number 2");
+// let number3 = +prompt("Enter number 3");
+// let number4 = +prompt("Enter number 4");
+// let number5 = +prompt("Enter number 5");
+// if (number1 > number2 && number1 > number3 && number1 > number4 && number1 > number5 ) {
+//     console.log("Your larger number is " + number1);
+// }   else if (number2 > number1 && number2 > number3 && number2 > number4 && number2 > number5 ) {
+//     console.log("Your larger number is " + number2);
+// }   else if (number3 > number1 && number3 > number2 && number3 > number4 && number3 > number5 ) {
+//     console.log("Your larger number is " + number3);
+// }   else if (number4 > number1 && number4 > number2 && number4 > number3 && number4 > number5 ) {
+//     console.log("Your larger number is " + number4);
+// }   else if (number5 > number1 && number5 > number2 && number5 > number3 && number5 > number4 ) {
+//     console.log("Your larger number is " + number5);
+// }   else {
+//     console.log("Try again and enter 5 diffrent number");
+// }
+
+
+// for (let i = 0; i <= 15; i++) {
+//     if( i % 2 == 0) {
+//         console.log(i + " is Even");
+//     }   else if (i % 2 !== 0) {
+//         console.log(i + " is Odd");
+//     }
+// }
+
+
+// let totalMarks = +prompt("Enter your total marks?");
+// let givenMarks = +prompt("Enter your given marks?");
+// let avgPer = (givenMarks / totalMarks) * 100;
+// if ( avgPer <= 100 && avgPer >= 90 ) {
+//     console.log("A grade. Your given percentage is: " + avgPer +"%");
+// }   else if ( avgPer < 90 && avgPer >= 80 ) {
+//     console.log("B grade. Your given percentage is: " + avgPer +"%");
+// }   else if ( avgPer < 80 && avgPer >= 70 ) {
+//     console.log("C grade. Your given percentage is: " + avgPer +"%");
+// }   else if ( avgPer < 70 && avgPer >= 60 ) {
+//     console.log("D grade. Your given percentage is: " + avgPer +"%");
+// }   else {
+//     console.log("F grade. Your given percentage is: " + avgPer +"%");
+// }
+
+
+// let givenPercentage = +prompt("Enter your percentage here.");
+// if (givenPercentage < 100 && givenPercentage >= 90) {
+//     console.log("A grade.");
+// }   else if (givenPercentage < 90 && givenPercentage >= 80) {
+//     console.log("B grade.");
+// }   else if (givenPercentage < 80 && givenPercentage >= 70) {
+//     console.log("C grade.");
+// }   else if (givenPercentage < 70 && givenPercentage >= 60) {
+//     console.log("D grade.");
+// }   else {
+//     console.log("F grade.");
+// }   
+
+
+// for (let i = 1; i < 100; i++) {
+//     if ( i % 3 == 0 && i % 5 == 0 ) {
+//         console.log("FizzBuzz");
+//     }   else if ( i % 3 == 0 ) {
+//         console.log("Fizz");
+//     }   else if ( i % 5 == 0 ) {
+//         console.log("Buzz");
+//     }
+// }
+
+
+// for (let i = 1; i <= 5; i++) {
+//    for (let j = 1; j <= i; j++) {
+//         document.write("*");
+//    }
+//    document.write("<br>");
+// }
